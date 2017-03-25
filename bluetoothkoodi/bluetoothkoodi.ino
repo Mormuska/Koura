876e9edf1286e95b5d7e78a3631eb2c04697d946
Niko Blomberg <Niko6141@gmail.com>
2017-03-25 23:43:41 +0200

    Selkeämpi koodi
    Kuittaa nyt käskyn selkeästi!    o7

--- bluetoothkoodi/bluetoothkoodi.ino
+++ bluetoothkoodi/bluetoothkoodi.ino
@@ -7,36 +7,36 @@ Servo myservo;
 Servo myservo2;
 
 void setup() {
-  // put your setup code here, to run once:
   myservo.attach(9);
   myservo2.attach(8);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if(Serial.available()){
     blueToothVal = Serial.readString();
-    //Serial.println(Serial.read());
-    Serial.println((blueToothVal));
     funktio(blueToothVal, nollakohdat);
   }
   delay(500);
 }
 
 void funktio(String blueVal,int nollakohdat[]){
+  //Ottaakäytäjän syötteen ja servoille kalibroidut nollakohdat, joissa ne pysähtyvät, ja lähettää nopeudet servoille.
+  //Syötteen ensimmäinen arvo kertoo käytettävän servon, loput nopeuden. Negatiivinen nopeus kääntää suuntaa.
+  
   int servo = int(blueVal.charAt(0)-'0');
   blueVal.remove(0,1);
   int nopeus = nollakohdat[servo] + blueVal.toInt();
-  Serial.println((blueVal));
-  Serial.println((nopeus));
+  
+  char text[100]; //Serialin kanssa pitää vähän kikkailla että saa testiarvot ulos.
+  snprintf(text, 100, "\nServon %d nopeus on %d", servo, nopeus);
+  Serial.println(text);
   
   //Servo member = a;   (  this doesnt work   :(  )
   //member.writeMicroseconds (nopeus);
   
   if(servo == 1){
     myservo.writeMicroseconds (nopeus);
-    Serial.println((blueVal));
     }
   if(servo == 2){
     myservo2.writeMicroseconds (nopeus);
